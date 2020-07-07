3f578a563041deb715b39fb33e473c4c1fe27783
Fredrik Jensen <post@fredrikjensen.com>
2020-07-07 22:27:52 +0200

    update babel

--- src/container.js
+++ src/container.js
@@ -2,16 +2,20 @@
  * Import dependencies
  */
 
-import React, { Component } from 'react';
-import { cloneDeep, range } from 'lodash';
-import uuid from 'uuid';
+import React, { Component } from "react";
+import { cloneDeep, range } from "lodash";
+import uuid from "uuid";
 
-import PatchEvent, { set, unset, setIfMissing } from 'part:@sanity/form-builder/patch-event';
+import PatchEvent, {
+  set,
+  unset,
+  setIfMissing,
+} from "part:@sanity/form-builder/patch-event";
 
-import { Heading } from './primitives';
-import Table from './table';
-import Button from 'part:@sanity/components/buttons/default'
-import ButtonGrid from 'part:@sanity/components/buttons/button-grid'
+import { Heading } from "./primitives";
+import Table from "./table";
+import Button from "part:@sanity/components/buttons/default";
+import ButtonGrid from "part:@sanity/components/buttons/button-grid";
 
 /**
  * Container
@@ -24,18 +28,15 @@ class Container extends Component {
     this.handleAddColumn = this.handleAddColumn.bind(this);
     this.handleCellChange = this.handleCellChange.bind(this);
 
-    const {
-      value: {
-        rows = [],
-      } = {},
-    } = props;
+    const { value: { rows = [] } = {} } = props;
 
     /**
      * Find out how many rows we have
      */
 
-    const columns = rows
-    .map((row) => !row ? 0 : !row.cells ? 0 : row.cells.length);
+    const columns = rows.map((row) =>
+      !row ? 0 : !row.cells ? 0 : row.cells.length
+    );
 
     /**
      * Set rows and columns
@@ -44,7 +45,7 @@ class Container extends Component {
     this.state = {
       columns: Math.max(...columns, 0),
       rows: rows.length,
-    }
+    };
   }
 
   /**
@@ -52,7 +53,7 @@ class Container extends Component {
    */
 
   handleAddRow(type) {
-    const { rows, columns } = this.state;
+    const { rows, columns } = this.state;
 
     this.setState({
       rows: rows + 1,
@@ -61,7 +62,7 @@ class Container extends Component {
   }
 
   handleAddColumn() {
-    const { columns, rows } = this.state;
+    const { columns, rows } = this.state;
 
     this.setState({
       columns: columns + 1,
@@ -70,82 +71,82 @@ class Container extends Component {
   }
 
   handleClear = () => {
-    const value = cloneDeep(this.props.value)
-    this.props.onChange(PatchEvent.from([
-      setIfMissing({ _type: 'table' }),
-      set({
-        ...value,
-        rows: value.rows.map(r => {
-          return {
-            ...r,
-            cells: r.cells.map(c => '')
-          }
-        })
-      })
-    ]))
-  }
+    const value = cloneDeep(this.props.value);
+    this.props.onChange(
+      PatchEvent.from([
+        setIfMissing({ _type: "table" }),
+        set({
+          ...value,
+          rows: value.rows.map((r) => {
+            return {
+              ...r,
+              cells: r.cells.map((c) => ""),
+            };
+          }),
+        }),
+      ])
+    );
+  };
 
   handleRemoveRow = (index) => {
-    const value = cloneDeep(this.props.value)
-    this.props.onChange(PatchEvent.from([
-      setIfMissing({ _type: 'table' }),
-      set({
-        ...value,
-        rows: value.rows.filter((r, i) => i !== index)
-      })
-    ]))
+    const value = cloneDeep(this.props.value);
+    this.props.onChange(
+      PatchEvent.from([
+        setIfMissing({ _type: "table" }),
+        set({
+          ...value,
+          rows: value.rows.filter((r, i) => i !== index),
+        }),
+      ])
+    );
     this.setState({
-      rows: this.state.rows - 1
-    })
-  }
+      rows: this.state.rows - 1,
+    });
+  };
 
   handleReset = (index) => {
-    const value = cloneDeep(this.props.value)
-    this.props.onChange(PatchEvent.from([
-      setIfMissing({ _type: 'table' }),
-      set({ ...value, rows: [] })
-    ]))
+    const value = cloneDeep(this.props.value);
+    this.props.onChange(
+      PatchEvent.from([
+        setIfMissing({ _type: "table" }),
+        set({ ...value, rows: [] }),
+      ])
+    );
     this.setState({
       rows: 0,
-      columns: 0
-    })
-  }
+      columns: 0,
+    });
+  };
 
   handleRemoveColumn = (index) => {
-    const value = cloneDeep(this.props.value)
-    this.props.onChange(PatchEvent.from([
-      setIfMissing({ _type: 'table' }),
-      set({
-        ...value,
-        rows: value.rows.map(r => {
-          return {
-            ...r,
-            cells: r.cells.filter((c, i) => i !== index)
-          }
-        })
-      })
-    ]))
+    const value = cloneDeep(this.props.value);
+    this.props.onChange(
+      PatchEvent.from([
+        setIfMissing({ _type: "table" }),
+        set({
+          ...value,
+          rows: value.rows.map((r) => {
+            return {
+              ...r,
+              cells: r.cells.filter((c, i) => i !== index),
+            };
+          }),
+        }),
+      ])
+    );
     this.setState({
-      columns: this.state.columns - 1
-    })
-  }
+      columns: this.state.columns - 1,
+    });
+  };
 
   /**
    * Add a patch event to Sanity
    */
 
   handleCellChange(e, row, column) {
-    const {
-      value: input,
-    } = e.target;
+    const { value: input } = e.target;
 
-    const {
-      onChange,
-      value,
-      type: {
-        name,
-      } = {},
-    } = this.props;
+    const { onChange, value, type: { name } = {} } = this.props;
 
     /**
      * Get all table data
@@ -157,19 +158,22 @@ class Container extends Component {
      * Convert all null values to strings
      */
 
-    const convertNulls = (rows = []) => range(rows.length).map(index => {
-      const row = rows[index];
+    const convertNulls = (rows = []) =>
+      range(rows.length).map((index) => {
+        const row = rows[index];
 
-      return ({
-        _type: row && row._type ? row._type : 'column',
-        _key: row && row._key ? row._key : uuid.v4(),
-        cells: row && row.cells ? range(row.cells.length)
-        .map(cellIndex => {
-          const cell = row.cells[cellIndex];
-          return cell ? cell : '';
-        }) : [],
-      })
-    });
+        return {
+          _type: row && row._type ? row._type : "column",
+          _key: row && row._key ? row._key : uuid.v4(),
+          cells:
+            row && row.cells
+              ? range(row.cells.length).map((cellIndex) => {
+                  const cell = row.cells[cellIndex];
+                  return cell ? cell : "";
+                })
+              : [],
+        };
+      });
 
     updatedData.rows = convertNulls(updatedData.rows);
 
@@ -179,7 +183,7 @@ class Container extends Component {
 
     if (!updatedData.rows[row]) {
       updatedData.rows[row] = {
-        _type: 'column',
+        _type: "column",
         _key: uuid.v4(),
         cells: [],
       };
@@ -201,34 +205,28 @@ class Container extends Component {
      * Add Sanity patch
      */
 
-    onChange(PatchEvent.from([
-      setIfMissing({_type: 'table'}),
-      updatedData ? set(updatedData) : unset(name),
-    ]));
+    onChange(
+      PatchEvent.from([
+        setIfMissing({ _type: "table" }),
+        updatedData ? set(updatedData) : unset(name),
+      ])
+    );
   }
 
   render() {
     const {
-      value: {
-        rows: rowsContent = [],
-      } = {},
-      type: {
-        title,
-      } = {},
+      value: { rows: rowsContent = [] } = {},
+      type: { title } = {},
     } = this.props;
 
-    const {
-      columns,
-      rows,
-    } = this.state;
+    const { columns, rows } = this.state;
 
     /**
      * Map data to multidimensional array
      * [[0, 1], [2, 3]]
      */
 
-    const tableData = rowsContent
-    .map(row => row && row.cells && row.cells);
+    const tableData = rowsContent.map((row) => row && row.cells && row.cells);
 
     /**
      * Render
@@ -247,12 +245,8 @@ class Container extends Component {
           onRemoveColumn={this.handleRemoveColumn}
         />
         <ButtonGrid>
-          <Button onClick={this.handleAddRow}>
-            Add row
-          </Button>
-          <Button onClick={this.handleAddColumn}>
-            Add column
-          </Button>
+          <Button onClick={this.handleAddRow}>Add row</Button>
+          <Button onClick={this.handleAddColumn}>Add column</Button>
           <Button onClick={this.handleClear} color="danger">
             Empty cells
           </Button>
